bf010e2a10c2bbdaab474ed07f3a35082a3ad9ff
Ansar <ansar.memon@hotmail.com>
2020-12-24 13:02:32 +0400

    fix(accessibility): Added alt tags to the images

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -4,7 +4,7 @@ const PostCard = ({ post }) => {
   return (
     <article className="border shadow rounded flex flex-col">
       <div className="relative w-full">
-        <Image src={post.image} width={1080} height={720}  layout="responsive" />
+        <Image src={post.image} width={1080} height={720} alt={post.text} layout="responsive" />
       </div>
       <div className="p-5 uppercase text-sm font-bold tracking-wide">
         {post.text} 
