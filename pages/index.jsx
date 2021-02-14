dd65dca86c274fcf6836c408ef0a3e4d1c78d24c
Ansar <ansar.memon@hotmail.com>
2021-02-14 06:58:56 +0400

    cleanup

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,13 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import useLocale from '../hooks/useLocale';
 
 export default function Home() {
   const { t } = useLocale();
   return (
-    <div className={styles.container}>
+    <div className="px-28">
       
-      <div className={styles.content}>
-        <h1>{t('welcome_to_the_demo', { name: 'Ansar' })}</h1>
+      <div className="my-24">
+        <h1 className="font-medium">{t('welcome_to_the_demo', { name: 'Ansar' })}</h1>
 
         <p>{t('lipsum_dummy_content')}</p>
       </div>
